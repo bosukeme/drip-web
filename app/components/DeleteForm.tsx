df17f204e5b612e741dd4252e8dbf417f137e978
bosukeme <ukeme.wilson@gmail.com>
2024-08-27 19:55:55 +0100

    adding files

---
+++ app/components/DeleteForm.tsx
@@ -0,0 +1,91 @@
+'use client'
+
+import { useState, FormEvent } from "react"
+import Image from "next/image"
+
+function DeleteForm() {
+    const [email, setEmail] = useState<string>("")
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        setIsLoading(true);
+
+        fetch("http://127.0.0.1:5001/USER-delete-user-email", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                user_email:email
+            })
+        })
+        .then(response => {
+
+            if (!response.ok) {
+                // Check if the response status is not OK (e.g., 404, 500)
+                return response.json().then(errorData => {
+                    throw new Error(errorData.message || "An error occurred");
+                });
+            }
+            return response.json();
+        
+        })
+        .then(data => {
+            alert("User Account has been deleted successful") 
+        })
+        .catch(error => {
+            alert("Unable to delete user: " + error.message)
+        })
+        .finally(() => {
+            setIsLoading(false); 
+            setEmail("");
+        });
+
+        setEmail("")
+
+    }
+
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <h3 
+                className="font-bold text-2xl text-center mb-5"
+            >
+                Delete User Account 
+            </h3>
+            <form
+                onSubmit={handleSubmit}
+                className="flex flex-col items-center justify-center border-2 rounded-lg py-10 px-8 w-full max-w-md"
+            >
+                <Image 
+                    src="/driptok.png"
+                    alt="driptok" 
+                    width={90}
+                    height={90}
+                    className="mb-10"
+                    priority
+                />
+
+                <input
+                    className="bg-slate-100 border h-10 w-full px-4 rounded-lg mb-5"
+                    type="email"
+                    placeholder="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)} 
+                    />
+                
+                <button
+                    type="submit"
+                    className={`bg-gradient-custom text-white font-bold py-2 px-4 rounded-md border-none cursor-pointer ${
+                        isLoading ? "opacity-50 cursor-not-allowed" : ""
+                    }`}
+                >
+                    {isLoading ? "Loading..." : "SUBMIT"}
+                </button>
+
+            </form>
+        </div>
+  )
+}
+
+export default DeleteForm
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import DeleteForm from "./components/DeleteForm";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen mx-auto p-24 bg-slate-50">
+      <DeleteForm />      
+    </main>
+  );
+}
